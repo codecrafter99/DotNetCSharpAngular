c572deb6dc13ebd9d0da59d27727ad60bf2cab86
Eric Bangerter <eric@Erics-MacBook-Pro.local>
2020-07-28 21:56:07 -0700

    Validated tokens for api call. added authorization

--- DotNetWeb.API/Controllers/ValuesController.cs
+++ DotNetWeb.API/Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DotNetWeb.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNetWeb.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -26,6 +28,7 @@ namespace DotNetWeb.API.Controllers
             return Ok(values);
         }
 
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
