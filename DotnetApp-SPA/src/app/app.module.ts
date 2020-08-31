7d3203d9d511325789ae135828cb7c137241f984
codecrafter99 <ebngrtr083@yahoo.com>
2020-08-30 20:55:32 -0700

    added routing

--- DotnetApp-SPA/src/app/app.module.ts
+++ DotnetApp-SPA/src/app/app.module.ts
@@ -11,20 +11,29 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      BrowserAnimationsModule
+      BrowserAnimationsModule,
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
      ErrorInterceptorProvider,
--- DotnetApp-SPA/src/app/nav/nav.component.ts
+++ DotnetApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -11,7 +13,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
-    private alertify: AlertifyService) { }
+    private alertify: AlertifyService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -23,6 +26,9 @@ export class NavComponent implements OnInit {
         this.alertify.success("logged in successfully");
       }, error => {
         this.alertify.error("login failed");
+      },
+      () => {
+        this.router.navigate(['/members']);
       }
     );
   }
@@ -34,6 +40,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message("logged out");
+    this.router.navigate(['./home']);
   }
 
 }
